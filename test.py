1f4878db03d5a96b7829ad69b8ff94a4150cdb50
hensby <whc0721a@outlook.com>
2020-06-10 17:14:25 -0700

    111

--- test.py
+++ test.py
@@ -3,14 +3,16 @@ import csv
 import app
 
 if __name__ == '__main__':
-
-    with open('data/people.csv', encoding='utf8') as r:
-        d = csv.reader(r, delimiter=',', skipinitialspace=True)
-        all_data_dict = {}
-        for k, line in enumerate(d):
-            if k == 0:
-                continue
-            all_data_dict[str(k)] = line
-
-
-    app.addCSV()
+    #
+    # with open('data/people.csv', encoding='utf8') as r:
+    #     d = csv.reader(r, delimiter=',', skipinitialspace=True)
+    #     all_data_dict = {}
+    #     for k, line in enumerate(d):
+    #         if k == 0:
+    #             continue
+    #         all_data_dict[str(k)] = line
+    #
+    #
+    # app.addCSV()
+    #
+    print(help())
